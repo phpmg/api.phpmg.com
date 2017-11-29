f36d42c453dc56ff56bab3e785c52567069ee009
Lucas Mezêncio <lucas.mezencio@gmail.com>
2017-11-29 11:13:10 -0200

    Add root endpoint with a 404 response

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Zend\Http\Request;
+
+/**
+ * Class DefaultController
+ *
+ * @package App\Controller
+ */
+class DefaultController
+{
+    /**
+     * @Route("/", name="index", methods="GET")
+     */
+    public function indexAction()
+    {
+        return new JsonResponse([
+            'method' => Request::METHOD_GET,
+            'url' => '/',
+            'errors' => [
+                [
+                'type' => 'unknown_error',
+                'message' => 'Unknown error',
+                'parameter_name' => null,
+                ],
+            ],
+        ], 404);
+    }
+}
